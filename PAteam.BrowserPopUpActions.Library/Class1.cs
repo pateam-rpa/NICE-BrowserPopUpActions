7da1d19e9b58b8cad21ef619b5088aaa59cbf8f6
Mateusz Majchrzak <mateusz@pateam.co>
2021-11-25 12:35:00 +0100

    Changes

--- PAteam.BrowserPopUpActions.Library/Class1.cs
+++ PAteam.BrowserPopUpActions.Library/Class1.cs
@@ -29,81 +29,6 @@ namespace Direct.BrowserPopUpActions.Library
             }
         }
 
-        public static string GetAUElementText(AutomationElement element)
-        {
-            object patternObj;
-            if (element.TryGetCurrentPattern(ValuePattern.Pattern, out patternObj))
-            {
-                var valuePattern = (ValuePattern)patternObj;
-                return valuePattern.Current.Value;
-            }
-            else if (element.TryGetCurrentPattern(TextPattern.Pattern, out patternObj))
-            {
-                var textPattern = (TextPattern)patternObj;
-                return textPattern.DocumentRange.GetText(-1).TrimEnd('\r');
-            }
-            else
-            {
-                return element.Current.Name;
-            }
-        }
-
-        private static AutomationElement GetRootAUElement()
-        {
-            LogDebug("Finding Root Element...");
-            return AutomationElement.RootElement;
-        }
-
-        private static AutomationElementCollection GetBrowserAUElements()
-        {
-            Condition BrowserPropertyCondition = new PropertyCondition(AutomationElement.ClassNameProperty, "Chrome_WidgetWin_1");
-            AutomationElement RootElement = GetRootAUElement();
-            if (RootElement == null)
-            {
-                LogDebug("Root element not found!");
-                return null;
-
-            }
-            LogDebug("Finding all browser elements...");
-            return RootElement.FindAll(TreeScope.Children, BrowserPropertyCondition);
-        }
-
-        private static AutomationElement GetPopUpAUElement()
-        {
-            Condition PopUpPropertyCondition = new PropertyCondition(AutomationElement.LocalizedControlTypeProperty, "dialog");
-            AutomationElementCollection BrowserAUElements = GetBrowserAUElements();
-            if (BrowserAUElements.Count == 0)
-            {
-                LogDebug("Browser elements not found! Please check if browser is open and active");
-                return null;
-            }
-
-            foreach(AutomationElement BrowserAUElement in BrowserAUElements)
-            {
-                AutomationElement PopUpAUElement = BrowserAUElement.FindFirst(TreeScope.Descendants, PopUpPropertyCondition);
-                if (PopUpAUElement != null)
-                {
-                    return PopUpAUElement;
-                }
-            }
-
-            LogDebug("Browser popup element not found. If this is incorrect, check if page with popup is acitve or try reloading and activate target page.");
-            return null;
-        }
-
-        private static bool GetPopUpExistence()
-        {
-            LogDebug("Finding browser popup element...");
-            AutomationElement PopUpAUElement = GetPopUpAUElement();
-
-            if (PopUpAUElement != null)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
         [DirectDom("Get PopUp Existence")]
         [DirectDomMethod("Get Browser PopUp Existence")]
         [MethodDescription("Iterates over all browser windows and evaluate if the popup notification could be found")]
@@ -180,6 +105,88 @@ namespace Direct.BrowserPopUpActions.Library
             {
                 LogError("Close Browser PopUp", e);
                 return false;
+            }
+        }
+
+        private static bool GetPopUpExistence()
+        {
+            LogDebug("Finding browser popup element...");
+            AutomationElement PopUpAUElement = GetPopUpAUElement();
+
+            if (PopUpAUElement != null)
+            {
+                LogDebug("Found dialog with localized control type: " + PopUpAUElement.Current.LocalizedControlType + " and name: " + PopUpAUElement.Current.Name);
+                return true;
+            }
+
+            return false;
+        }
+
+        private static AutomationElement GetPopUpAUElement()
+        {
+            AutomationElementCollection BrowserAUElements = GetBrowserAUElements();
+            if (BrowserAUElements.Count == 0)
+            {
+                LogDebug("Browser elements not found! Please check if browser is open and active");
+                return null;
+            }
+
+            LogDebug("BrowserAUElements Count: " + BrowserAUElements.Count.ToString());
+            Condition PopUpPropertyCondition = new PropertyCondition(AutomationElement.LocalizedControlTypeProperty, "dialog");
+            Condition BrowserRootPropertyCondition = new PropertyCondition(AutomationElement.ClassNameProperty, "BrowserRootView");
+            foreach (AutomationElement BrowserAUElement in BrowserAUElements)
+            {
+                AutomationElement BrowserRootAUElement = BrowserAUElement.FindFirst(TreeScope.Children, BrowserRootPropertyCondition);
+                if (BrowserRootAUElement != null)
+                {
+                    AutomationElement PopUpAUElement = BrowserRootAUElement.FindFirst(TreeScope.Children, PopUpPropertyCondition);
+                    if (PopUpAUElement != null)
+                    {
+                        return PopUpAUElement;
+                    }
+                }
+            }
+
+            LogDebug("Browser popup element not found. If this is incorrect, check if page with popup is acitve or try reloading and activate target page.");
+            return null;
+        }
+
+        private static AutomationElementCollection GetBrowserAUElements()
+        {
+            Condition BrowserPropertyCondition = new PropertyCondition(AutomationElement.ClassNameProperty, "Chrome_WidgetWin_1");
+            AutomationElement RootElement = GetRootAUElement();
+            if (RootElement == null)
+            {
+                LogDebug("Root element not found!");
+                return null;
+
+            }
+            LogDebug("Finding all browser elements...");
+            return RootElement.FindAll(TreeScope.Children, BrowserPropertyCondition);
+        }
+
+        private static AutomationElement GetRootAUElement()
+        {
+            LogDebug("Finding Root Element...");
+            return AutomationElement.RootElement;
+        }
+
+        public static string GetAUElementText(AutomationElement element)
+        {
+            object patternObj;
+            if (element.TryGetCurrentPattern(ValuePattern.Pattern, out patternObj))
+            {
+                var valuePattern = (ValuePattern)patternObj;
+                return valuePattern.Current.Value;
+            }
+            else if (element.TryGetCurrentPattern(TextPattern.Pattern, out patternObj))
+            {
+                var textPattern = (TextPattern)patternObj;
+                return textPattern.DocumentRange.GetText(-1).TrimEnd('\r');
+            }
+            else
+            {
+                return element.Current.Name;
             }
         }
     }
